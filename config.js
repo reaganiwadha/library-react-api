f5031149534619c2dd44cf2db8db74e87cb5a1cd
reaganiwadha <reaganiwadha@protonmail.com>
2020-01-02 15:33:50 +0700

    Switched from DB to DATABASE

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const {Pool} = require('pg');
 
 const connectionString = 
-process.env.DB_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
     connectionString : connectionString,
